9589db89984f8dacae4b5dffeba2e5b2f8e72e93
abdalla-amin <abdalla.amin@vodafone.com>
2022-12-27 22:20:02 +0200

    update post api after deployment

--- client/script.js
+++ client/script.js
@@ -96,7 +96,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('http://127.0.0.1:5000', {
+  const response = await fetch('https://chategypt.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
